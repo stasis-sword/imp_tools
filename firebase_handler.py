bef6c78b9104f49592caf0adec163bd80e53a0f3
Arch Nemesis <archnemesissa@gmail.com>
2023-09-15 18:30:08 -0700

    add start/end time parsing to trophy collector

--- firebase_handler.py
+++ firebase_handler.py
@@ -23,28 +23,74 @@ class FirebaseHandler:
         # this could cause a problem if run right after the year ends...
         self.year = datetime.now().astimezone(pytz.timezone('US/Eastern')).year
 
+    def get_time_window(self, game_or_event_doc):
+        properties = game_or_event_doc.to_dict()
+        default_start_time = datetime(
+            self.year, 1, 1, 0, 0, 0, 0
+        ).astimezone(pytz.timezone('US/Eastern'))
+        default_end_time = datetime(
+            self.year + 1, 1, 1, 0, 0, 0, 0
+        ).astimezone(pytz.timezone('US/Eastern'))
+
+        if properties.get('startTime') and properties.get('endTime'):
+            start_time = properties['startTime']
+            end_time = properties['endTime']
+            return start_time, end_time
+        if properties.get('hidden'):
+            # hidden game or event - use default time window for year. Should
+            # this behavior be different?
+            return default_start_time, default_end_time
+
+        # game or event doesn't have a time window set. Check for umbrella
+        # event with time window
+        if not properties.get('event'):
+            print(f'*** Warning! {game_or_event_doc.id} has no start ' +
+                  'and/or end time and no associated umbrella event. No ' +
+                  'time window will be applied for its trophies.')
+            return default_start_time, default_end_time
+
+        event_doc = properties['event'].get()
+        event = event_doc.to_dict()
+        if event.get('startTime') and event.get('endTime'):
+            start_time = event['startTime']
+            end_time = event['endTime']
+            return start_time, end_time
+
+        print(f'*** Warning! {game_or_event_doc.id} is associated ' +
+              f'with event {event.id}, which does not have a start ' +
+              'and/or end time. No time window will be applied for ' +
+              'its trophies.')
+        return default_start_time, default_end_time
+
     def get_firebase_trophies_by_collection_type(
             self, collection_name, year_property):
         trophy_dict = {}
 
         doc_stream = self.db_client.collection(collection_name).stream()
         for doc in doc_stream:
-            properties = doc.to_dict()
             # only gather trophies from games/events from this year
-            if properties[year_property] != self.year:
+            properties = doc.to_dict()
+            doc_year = properties.get(year_property)
+            if not doc_year:
+                print(f'*** Warning! No year entered for {doc.id}. Trophies ' +
+                      'will never be gathered.')
                 continue
+            if doc_year != self.year:
+                continue
+
+            start_time, end_time = self.get_time_window(doc)
 
             path = doc.reference.path
             trophy_docs = self.db_client.collection(path + '/trophies')
             for trophy_doc in trophy_docs.stream():
-                game = doc.id
-                name = trophy_doc.id
-                image_url = trophy_doc.to_dict()['imageUrl']
                 trophy_data = {
-                    "game": game,
-                    "name": name,
-                    'reference': trophy_doc.reference
+                    "game": doc.id,
+                    "name": trophy_doc.id,
+                    'reference': trophy_doc.reference,
+                    "start_time": start_time,
+                    "end_time": end_time
                 }
+                image_url = trophy_doc.to_dict()['imageUrl']
                 trophy_dict[image_url] = trophy_data
 
         return trophy_dict
